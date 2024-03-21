4d819cd63ad026afec87b8dbb8563c903cff617d
maram.khalfaoui <maram.khalfaoui@etudiant-enit.utm.tn>
2024-03-21 01:33:31 +0100

    test2.0

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-const CustomFooter = styled.footer`
+const CustomFooterWrapper = styled.footer`
   position: absolute;
   bottom: 0;
   width: 100%;
   line-height: 60px;
   background-color: #f5f5f5;
+
+  &.bg-dark {
+    background-color: #343a40; /* Adjust background color as needed */
+    color: #ffffff; /* Adjust text color as needed */
+  }
 `;
 
-export const Footer = () => {
+export const Footer = ({ className }) => {
   return (
-    <CustomFooter className="bg-dark">
+    <CustomFooterWrapper className={className}>
       <div className="container">
         <span className="text-white">
           &#169;
@@ -27,7 +32,7 @@ export const Footer = () => {
           </a>
         </div>
       </div>
-    </CustomFooter>
+    </CustomFooterWrapper>
   );
 };
 
